fe36e03c9f8b0c0d803e93ec12c6706b75e3e1bf
Dulatbay <adulatbai@bk.ru>
2024-01-25 13:50:06 +0500

    solve bag

--- src/main/java/kz/miniland/minilandserver/security/CustomCorsFilter.java
+++ src/main/java/kz/miniland/minilandserver/security/CustomCorsFilter.java
@@ -4,10 +4,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 public class CustomCorsFilter extends OncePerRequestFilter {
 
     @Override

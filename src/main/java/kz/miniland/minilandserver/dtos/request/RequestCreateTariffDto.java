2d97f55961946374955ab63317967450c5d3059b
Dulatbay <adulatbai@bk.ru>
2024-02-07 08:22:48 +0600

    add: create RequestCreateTariffDto

---
+++ src/main/java/kz/miniland/minilandserver/dtos/request/RequestCreateTariffDto.java
@@ -0,0 +1,30 @@
+package kz.miniland.minilandserver.dtos.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import lombok.Data;
+
+import java.time.LocalTime;
+import java.util.Set;
+
+@Data
+public class RequestCreateTariffDto {
+    @JsonProperty("started_at")
+    private LocalTime startedAt;
+    @JsonProperty("finished_at")
+    private LocalTime finishedAt;
+    @JsonProperty("days")
+    private Set<Integer> days;
+    @JsonProperty("first_price")
+    @Min(value = 0, message = "Price must be more than 0")
+    private Double firstPrice;
+    @JsonProperty("penalty_per_hour")
+    @Min(value = 0, message = "Penalty must be more than 0")
+    private Double penaltyPerHour;
+    @JsonProperty("penalty_per_half_hour")
+    @Min(value = 0, message = "Penalty must be more than 0")
+    private Double penaltyPerHalfHour;
+    @JsonProperty("max_child")
+    @Min(value = 0, message = "The number of children must be more than 3")
+    private Integer maxChild;
+}

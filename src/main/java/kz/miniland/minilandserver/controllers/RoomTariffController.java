0d9e23ba1dfc138308afc350b1b27ee110bc499f
Dulatbay <adulatbai@bk.ru>
2024-02-07 12:26:41 +0600

    fix: remove unused import statement

--- src/main/java/kz/miniland/minilandserver/controllers/RoomTariffController.java
+++ src/main/java/kz/miniland/minilandserver/controllers/RoomTariffController.java
@@ -9,8 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.PathParam;
 import java.util.List;
 
 @RestController

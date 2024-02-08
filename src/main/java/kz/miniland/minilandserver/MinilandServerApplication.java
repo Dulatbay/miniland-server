e3d2cae757350253c9db9d59808992ad01740360
Dulatbay <adulatbai@bk.ru>
2024-02-08 19:08:50 +0600

    fix: change deleteAllFiles to deleteAllFiles by params

--- src/main/java/kz/miniland/minilandserver/MinilandServerApplication.java
+++ src/main/java/kz/miniland/minilandserver/MinilandServerApplication.java
@@ -1,11 +1,28 @@
 package kz.miniland.minilandserver;
 
+import kz.miniland.minilandserver.services.FileService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MinilandServerApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MinilandServerApplication.class, args);
-	}
+
+    @Value("${application.delete-all-files}")
+    private Boolean deleteAllFiles;
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinilandServerApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(FileService fileService) {
+        return (args) -> {
+            if (deleteAllFiles)
+                fileService.deleteAll();
+            fileService.init();
+        };
+    }
 }

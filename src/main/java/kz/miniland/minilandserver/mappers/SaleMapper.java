ce6b24cfa75e36fca4ef74e37992c8c11c24b2db
Beimbet <beimbet.maratov@mail.ru>
2024-02-16 18:29:45 +0600

    fix: hot bug fix

--- src/main/java/kz/miniland/minilandserver/mappers/SaleMapper.java
+++ src/main/java/kz/miniland/minilandserver/mappers/SaleMapper.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 public interface SaleMapper extends BaseMapper<Sale, ResponseSaleDto> {
     @Override
     default ResponseSaleDto toDto(Sale entity) {
+        if(entity == null) return new ResponseSaleDto();
         ResponseSaleDto saleDto = new ResponseSaleDto();
         saleDto.setId(entity.getId());
         saleDto.setEnabled(entity.isEnabled());
@@ -25,6 +26,8 @@ public interface SaleMapper extends BaseMapper<Sale, ResponseSaleDto> {
 
     @Override
     default Sale toEntity(ResponseSaleDto responsePriceDto) {
+        if(responsePriceDto == null)
+            return new Sale();
         Sale sale = new Sale();
         sale.setId(responsePriceDto.getId());
         sale.setEnabled(responsePriceDto.getEnabled());

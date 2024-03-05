56ac7547d4bdc3df2f652cc7237eecd2e10bd50d
luciorim <88190686+luciorim@users.noreply.github.com>
2024-03-06 02:34:20 +0500

    fix: changed time zone to UTC+5

--- src/main/java/kz/miniland/minilandserver/constants/ValueConstants.java
+++ src/main/java/kz/miniland/minilandserver/constants/ValueConstants.java
@@ -7,7 +7,7 @@ import java.time.ZoneId;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ValueConstants {
-    public static ZoneId ZONE_ID = ZoneId.of("UTC+06:00"); // Almaty, Kazakhstan
+    public static ZoneId ZONE_ID = ZoneId.of("UTC+05:00"); // Almaty, Kazakhstan
     public static String KEYCLOAK_REALM = "miniland";
     public static String UPLOADED_FOLDER = "upload-dir/";
 }
